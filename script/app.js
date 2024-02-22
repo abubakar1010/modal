a1c897d10b6d147844c388d5e6c5186e9c3de561
Abubakar1010 <abubakar850772@gmail.com>
2024-02-23 00:51:53 +0600

    finished project

--- script/app.js
+++ script/app.js
@@ -1,1 +1,28 @@
-varia
+// variable
+
+const openBtn = document.querySelector(".openBtn")
+const closeBtn = document.querySelector(".closeBtn")
+const container = document.querySelector(".container")
+
+// addEventListener
+
+openBtn.addEventListener('click', function(){
+
+    container.style.display = "block"
+
+})
+closeBtn.addEventListener('click', function(){
+
+    container.style.display = "none"
+
+})
+
+window.addEventListener('click', function(e){
+
+    if( e.target === container){
+
+        container.style.display = "none"
+    }
+
+
+})
